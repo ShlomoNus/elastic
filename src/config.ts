dd7fcd36f85b35f834fc7962b14cc6178dd0ac04
ShlomoNus <shlomonuswork@gmail.com>
2025-03-25 16:19:54 +0200

    setup

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,9 @@
 import { loadEnvFiles } from '@utils/vars';
-import { cleanEnv, str, url, num } from 'envalid';
+import { cleanEnv, str, num } from 'envalid';
 
 loadEnvFiles();
 
 export const CONFIG = cleanEnv(process.env, {
-    BASE_URL: url({ devDefault: 'mongodb://127.0.0.1:27017/' }),
     DB_NAME: str({ devDefault: 'localUsers' }),
     Port: num({ devDefault: 5050 }),
     Secret: str({ devDefault: '12345' }),
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,14 +1,10 @@
-import { login, signup } from '@handlers/auth';
 import { applyRoutes } from '@utils/backend';
 import { Router } from 'express';
 import { Route } from 'sn-types-backend';
 
 const userRouter = Router();
 
-export const routes: Route[] = [
-    { path: '/login', method: 'post', handler: login },
-    { path: '/signup', method: 'post', handler: signup },
-];
+export const routes: Route[] = [];
 
 applyRoutes({ app: userRouter, routes });
 
